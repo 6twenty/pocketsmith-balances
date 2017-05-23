4da9aa1fab5a26d823ef0ef8d43445293e6d6d02
Mike Fulcher <michael.fulcher@gmail.com>
2017-05-23 12:03:43 +1200

    Implement app
    - Fetches user info (to get ID) and then account info
    - Renders each account with title, balance date, and balance
    - Includes currency code if multicurrency

--- main.js
+++ main.js
@@ -1,11 +1,114 @@
-const electron = require('electron')
-const menubar = require('menubar')
+const got = require('got')
+const path = require('path')
+const {app, BrowserWindow, ipcMain, Tray, nativeImage} = require('electron')
+const {apiKey} = require('./config')
 
-// Module to control application life.
-const app = electron.app
+let tray
+let window
 
-app.log = (...args) => {
-  console.log(...args)
+app.on('ready', _ => {
+  if (app.dock) app.dock.hide()
+
+  // Setup the menubar with an icon
+  tray = new Tray(path.join(__dirname, 'IconTemplate.png'))
+
+  tray.setHighlightMode('never')
+
+  // Add a click handler so that when the user clicks on the menubar icon, it shows
+  // our popup window
+  tray.on('click', function(event) {
+    toggleWindow()
+
+    // Show devtools when command clicked
+    if (window.isVisible() && process.defaultApp && event.ctrlKey) {
+      window.openDevTools({ mode: 'detach' })
+    }
+  })
+
+  // Make the popup window for the menubar
+  window = new BrowserWindow({
+    width: 300,
+    height: 300,
+    show: false,
+    frame: false,
+    resizable: false,
+    transparent: true,
+    shadow: false
+  })
+
+  // Tell the popup window to load our index.html file
+  window.loadURL(`file://${path.join(__dirname, 'index.html')}`)
+
+  // Only close the window on blur if dev tools isn't opened
+  window.on('blur', _ => {
+    tray.setHighlightMode('never')
+    if (!window.webContents.isDevToolsOpened()) {
+      window.hide()
+    }
+  })
+
+  window.on('hide', _ => {
+    tray.setHighlightMode('never')
+  })
+
+  window.on('focus', _ => {
+    tray.setHighlightMode('always')
+  })
+
+  window.on('show', _ => {
+    tray.setHighlightMode('always')
+    window.webContents.send('refresh')
+  })
+})
+
+const toggleWindow = _ => {
+  if (window.isVisible()) {
+    window.hide()
+  } else {
+    showWindow()
+  }
 }
 
-const mb = menubar()
+const showWindow = _ => {
+  const trayBounds = tray.getBounds()
+  const windowBounds = window.getBounds()
+
+  let x = Math.round(trayBounds.x + (trayBounds.width / 2) - (windowBounds.width / 2))
+  let y = Math.round(trayBounds.y + trayBounds.height)
+
+  window.setPosition(x, y, false)
+  window.show()
+  window.focus()
+}
+
+const get = endpoint => {
+  return got(`https://api.pocketsmith.com/v2/${endpoint}`, {
+    json: true, headers: { 'Authorization': `Key ${apiKey}` }
+  }).then(response => {
+    return response.body
+  })
+}
+
+ipcMain.on('log', (event, message) => {
+  console.log(message)
+})
+
+ipcMain.on('get', (event, endpoint) => {
+  get(endpoint).then(json => {
+    event.sender.send('get-success', json)
+  }).catch(error => {
+    event.sender.send('get-fail', error)
+  })
+})
+
+ipcMain.on('resize', (event, {width, height}, animate) => {
+  const bounds = window.getBounds()
+  const offset = (width - bounds.width) / 2
+
+  window.setBounds({
+    width: width,
+    height: height,
+    x: bounds.x - offset,
+    y: bounds.y
+  }, animate)
+})
--- renderer.js
+++ renderer.js
@@ -1,1 +1,72 @@
-const {log} = require('electron').remote.app
+// In renderer process (web page).
+const {ipcRenderer} = require('electron')
+const accounting = require('accounting-js')
+const moment = require('moment')
+
+const log = message => {
+  ipcRenderer.send('log', message)
+}
+
+const get = endpoint => {
+  const channelSuccess = `get-success`
+  const channelFail = `get-fail`
+
+  const done = _ => {
+    ipcRenderer.removeAllListeners(channelSuccess)
+    ipcRenderer.removeAllListeners(channelFail)
+    return true
+  }
+
+  return new Promise((resolve, reject) => {
+    ipcRenderer.once(channelSuccess, (event, data) => done() && resolve(data))
+    ipcRenderer.once(channelFail, (event, error) => done() && reject(error))
+    ipcRenderer.send('get', endpoint)
+  })
+}
+
+const renderAccounts = (user, accounts) => {
+  const main = document.querySelector('main')
+  const content = main.querySelector('.content')
+
+  log('Rendering...')
+
+  // Remove any existing markup first
+  content.querySelectorAll('.account').forEach(el => {
+    content.removeChild(el)
+  })
+
+  accounts.forEach(account => {
+    const el = document.createElement('div')
+    const title = document.createElement('div')
+    const meta = document.createElement('div')
+    const balance = document.createElement('div')
+    const date = moment(account.current_balance_date, 'YYYY-MM-DD').format('MMM D, YYYY')
+    const amount = accounting.formatMoney(account.current_balance, {
+      symbol: account.currency_code,
+      format: user.using_multiple_currencies ? "%v %s" : "%v"
+    })
+
+    el.classList.add('account')
+    title.classList.add('title')
+    meta.classList.add('meta')
+    balance.classList.add('balance')
+
+    title.appendChild(document.createTextNode(account.title))
+    meta.appendChild(document.createTextNode(date))
+    balance.appendChild(document.createTextNode(amount))
+
+    el.appendChild(title)
+    el.appendChild(meta)
+    el.appendChild(balance)
+    content.appendChild(el)
+  })
+}
+
+const refresh = _ => {
+  log('Refreshing...')
+  get('me').then(json => get(`users/${json.id}/accounts`).then(renderAccounts.bind(this, json)))
+}
+
+refresh()
+
+ipcRenderer.on('refresh', refresh)
